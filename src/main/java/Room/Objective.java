6e3ed7c4ecc8ff05d73c1601504ce78d4c780b85
Marijke Drenth <drenth.marijke@googlemail.com>
2022-02-09 17:36:55 +0200

    Add getters and setters to objectives

--- src/main/java/Room/Objective.java
+++ src/main/java/Room/Objective.java
@@ -7,8 +7,24 @@ public abstract class Objective implements IObjective {
     private String type;
     private int xP;
 
-    public Objective(String type, int xP) {
+    public Objective(int xP) {
+        this.type = "";
+        this.xP = xP;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
         this.type = type;
+    }
+
+    public int getxP() {
+        return xP;
+    }
+
+    public void setxP(int xP) {
         this.xP = xP;
     }
 }
