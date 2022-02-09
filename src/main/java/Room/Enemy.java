36433106e4ef84e2c9acda97d74467e0c0aa5fc0
Marijke Drenth <drenth.marijke@googlemail.com>
2022-02-09 17:55:28 +0200

    Add enemy

--- src/main/java/Room/Enemy.java
+++ src/main/java/Room/Enemy.java
@@ -1,8 +1,31 @@
 package Room;
 
-public abstract class Enemy extends Objective{
-    public Enemy(String type, int xP) {
-        super(xP);
+import interfaces.IAttack;
 
+public abstract class Enemy extends Objective implements IAttack {
+
+    private int health;
+    private int damageInflicted;
+
+    public Enemy(int xP,int health, int damageInflicted) {
+        super(xP);
+        this.health = health;
+        this.damageInflicted = damageInflicted;
+    }
+
+    public int getHealth() {
+        return health;
+    }
+
+    public void setHealth(int health) {
+        this.health = health;
+    }
+
+    public int getDamageInflicted() {
+        return damageInflicted;
+    }
+
+    public void setDamageInflicted(int damageInflicted) {
+        this.damageInflicted = damageInflicted;
     }
 }
